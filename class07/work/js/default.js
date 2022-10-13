8888d2f6365f0cf5d86acc084a0a484d3bb3770b
lee <phinpig@gmail.com>
2022-10-13 17:44:36 +0900

    게임

--- class07/work/js/default.js
+++ class07/work/js/default.js
@@ -5,125 +5,140 @@
  * 성공/실패시 나오는 창 그리고 소리
  */
 const picture = document.querySelector('.postion-area');
-const timeArea = document.querySelector('.time');
+const displayTime = document.querySelector('.time');
 const startButton = document.querySelector('.btn-start');
 const stopButton = document.querySelector('.btn-stop');
 const displayPickUp = document.querySelector('.pickup');
+const replay = document.querySelector('.replay');
+const popup = document.querySelector('.popup');
 const picWidth = picture.offsetWidth;
 const picHeight = picture.offsetHeight;
+const timer = 10;
+const bugs = 10;
+const carrots = 8;
+let displayMsg = document.querySelector('.msg');
 let time = false;
 let currentPlay = true;
+
 startButton.addEventListener('click', startGame);
 stopButton.addEventListener('click', stopAlert);
 picture.addEventListener('click', pickUpCarrot);
+replay.addEventListener('click', replayStart);
 
 class SoundSetting {
-  constructor() {
-    this.bg = new Audio('sound/bg.mp3');
-    this.fail = new Audio('sound/alert.wav');
-    this.bug = new Audio('sound/bug_pull.mp3');
-    this.carrot = new Audio('sound/carrot_pull.mp3');
-    this.success = new Audio('sound/game_win.mp3');
-  }
+    constructor() {
+        this.bg = new Audio('sound/bg.mp3');
+        this.fail = new Audio('sound/alert.wav');
+        this.bug = new Audio('sound/bug_pull.mp3');
+        this.carrot = new Audio('sound/carrot_pull.mp3');
+        this.success = new Audio('sound/game_win.mp3');
+    }
 }
 const sound = new SoundSetting();
 
 // 타이머 설정
 function countWatch(ele) {
-  viewTime(ele);
-  time = setInterval(() => {
-    if (!ele) {
-      //0이면 실패
-      msgAlert(false);
-    }
-    viewTime(ele--);
-  }, 1000);
+    viewTime(ele);
+    time = setInterval(() => {
+        if (!ele) {
+            //0이면 실패
+            msgAlert(false);
+        }
+        viewTime(ele--);
+    }, 1000);
 }
 // 시간 보이기
 
 function viewTime(ele) {
-  timeArea.innerHTML = '00:' + new String(ele).padStart(2, 0);
+    displayTime.innerHTML = '00:' + new String(ele).padStart(2, 0);
 }
 
 function startGame() {
-  currentPlay = true;
-  startButton.style.display = 'none';
-  stopButton.style.display = 'block';
-  while (picture.hasChildNodes()) {
-    picture.removeChild(picture.firstChild);
-  }
-  countWatch(4); // 남은 시간(초)
-  randomItem(2, 7); // 당근 수, 벌레 수
-  sound.bg.play();
+    currentPlay = true;
+    startButton.style.display = 'none';
+    stopButton.style.display = 'block';
+    while (picture.hasChildNodes()) {
+        picture.removeChild(picture.firstChild);
+    }
+    countWatch(timer); // 남은 시간(초)
+    randomItem(bugs, carrots); // 당근 수, 벌레 수
+    sound.bg.play();
 }
 
 // 랜덤하게 벌레/당근 위치시키기
 function randomItem(carrots = 10, bugs = 5) {
-  //당근
-  for (let i = 0; i < carrots; i++) {
-    insertItem();
-  }
-  //벌레
-  for (let i = 0; i < bugs; i++) {
-    insertItem(false);
-  }
-  displayPickUp.innerText = carrots;
+    //당근
+    for (let i = 0; i < carrots; i++) {
+        insertItem();
+    }
+    //벌레
+    for (let i = 0; i < bugs; i++) {
+        insertItem(false);
+    }
+    displayPickUp.innerText = carrots;
 }
 
 function insertItem(carrotYn = true) {
-  const pos = {
-    //  아이템들이 랜덤하게 위치할 영역값을 가지고 옴
-    x: Math.round(Math.random() * picWidth),
-    y: Math.round(Math.random() * picHeight),
-  };
-  const img = document.createElement('img');
-  const imgSrc = carrotYn ? 'img/carrot.png' : 'img/bug.png';
-  img.setAttribute('src', imgSrc);
-  img.setAttribute('data-id', carrotYn);
-  img.setAttribute('style', `top:${pos.y}px;left:${pos.x}px`);
+    const pos = {
+        //  아이템들이 랜덤하게 위치할 영역값을 가지고 옴
+        x: Math.round(Math.random() * picWidth),
+        y: Math.round(Math.random() * picHeight),
+    };
+    const img = document.createElement('img');
+    const imgSrc = carrotYn ? 'img/carrot.png' : 'img/bug.png';
+    img.setAttribute('src', imgSrc);
+    img.setAttribute('data-id', carrotYn);
+    img.setAttribute('style', `top:${pos.y}px;left:${pos.x}px`);
 
-  picture.append(img);
+    picture.append(img);
 }
 
 // 당근/벌레 클릭시 (숫자빼기, 당근 없애기, 벌레 선택시 실패)
 function pickUpCarrot(e) {
-  const obj = e.target;
-  let num = displayPickUp.innerText;
-  // currentPlay :true 게임중, false:게임실패 / 승리
-  if (obj.tagName === 'IMG' && currentPlay) {
-    if (eval(obj.dataset.id)) {
-      e.target.remove();
-      sound.carrot.play();
-      num--;
-      displayPickUp.innerText = num;
-      if (num === 0) {
-        msgAlert(true);
-      }
-    } else {
-      sound.bug.play();
-      msgAlert(false);
+    const obj = e.target;
+    let num = displayPickUp.innerText;
+    // currentPlay :true 게임중, false:게임실패 / 승리
+    if (obj.tagName === 'IMG' && currentPlay) {
+        if (eval(obj.dataset.id)) {
+            e.target.remove();
+            sound.carrot.play();
+            num--;
+            displayPickUp.innerText = num;
+            if (num === 0) {
+                msgAlert(true, 'You Win');
+            }
+        } else {
+            sound.bug.play();
+            msgAlert(false, 'Yout Lost');
+        }
     }
-  }
 }
 
 // 성공/실패시 나오는 창, 닫기
-function msgAlert(type) {
-  currentPlay = false;
-  sound.bg.pause();
-  stopButton.style.display = 'none';
-  clearInterval(time);
+function msgAlert(type, msg) {
+    currentPlay = false;
+    sound.bg.pause();
+    stopButton.style.display = 'none';
+    clearInterval(time);
 
-  if (type) {
-    sound.success.play();
-    console.log('성공팝업');
-  } else {
-    sound.fail.play();
-    console.log('실패팝업');
-  }
+    if (type) {
+        sound.success.play();
+    } else {
+        sound.fail.play();
+    }
+    popup.style.visibility = 'visible';
+    displayMsg.innerText = msg;
 }
 function stopAlert() {
-  msgAlert(false);
+    msgAlert(false, 'Replay ❓');
+}
+function replayStart() {
+    popup.style.visibility = 'hidden';
+    displayMsg.innerText = '';
+    displayTime.innerText = '00:00';
+    displayPickUp.innerText = '0';
+    startButton.style.display = 'block';
 }
 function closeAlert() {
-  alert('닫기');
+    alert('닫기');
 }
